eefaf17d72ec25f967d5bdbd12c480c51000e887
Max Tymchii <max@ekreative.com>
2012-08-20 16:26:31 +0300

    Implement jotAutograph to controller.

--- Autograph/Autograph/jotViewController.m
+++ Autograph/Autograph/jotViewController.m
@@ -7,25 +7,30 @@
 //
 
 #import "jotViewController.h"
-#import "SmoothLineView.h"
-
-
-@interface jotViewController ()
+#import "jotAutographView.h"
+@interface jotViewController ()<jotAutographViewDelegate>
+@property (weak, nonatomic)  UIImageView *showAutograph;
 
 @end
 
 @implementation jotViewController
+@synthesize showAutograph;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-     [self.view addSubview:[[SmoothLineView alloc] initWithFrame:self.view.bounds]];
-    
+    UIImageView * autograpPicture = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    self.showAutograph = autograpPicture;
+    [self.view addSubview:self.showAutograph];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    [self.view addSubview:[[jotAutographView alloc] initWithSuperFrame:self.view.frame theDelegate:self theSize:CGSizeMake(500, 500) andTitle:@"Total price: 400 pounds."]];
 }
+                           
 
 - (void)viewDidUnload
 {
+    [self setShowAutograph:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     
@@ -38,6 +43,12 @@
     } else {
         return YES;
     }
+}
+
+#pragma mark - jotAutographViewDelegate
+-(void) autographImage:(UIImage *) autograph
+{
+    self.showAutograph.image = autograph;
 }
 
 @end
